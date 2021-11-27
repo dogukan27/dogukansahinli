853f05b8d98e9c49d807461642d3390b0e7bd34a
Doğukan Şahinli <44443599+dogukan27@users.noreply.github.com>
2021-11-27 21:01:05 +0300

    Update ProductsController.cs

--- shopapp/shopapp.webapi/Controllers/ProductsController.cs
+++ shopapp/shopapp.webapi/Controllers/ProductsController.cs
@@ -8,10 +8,57 @@ using Microsoft.Extensions.Logging;
 namespace shopapp.webapi.Controllers
 {
     [ApiController]
-    //localhost:4200/api/products (api olan yer zorunlu değil)
     [Route("api/[controller]")]
     public class ProductsController:ControllerBase
     {
-        
+         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
+        [HttpGet("[action]")]
+        public IActionResult GetCategories()
+        {
+            return Ok(cm.List());
+        }
+
+
+
+        [HttpGet("[action]/{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            return Ok(cm.GetById(id));
+        }
+
+
+
+        [HttpPut("{id}")]
+        //api/values/1
+        public IActionResult UpdateCategory(int id,Category category)
+        {
+            var cat = cm.GetById(id);
+            cat.Name = category.Name;
+            cat.Description = category.Description;
+            cat.CategoryStatus = category.CategoryStatus;
+            cm.Update(cat);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        //api/values/1
+        public IActionResult DeleteCategory(int id)
+        {
+            var deletedcat = cm.GetById(id);
+            cm.Delete(deletedcat);
+            return NoContent();
+        }
+
+
+        [HttpPost]
+
+        public IActionResult AddCategory(Category category)
+        {
+            cm.Insert(category);
+            return Created("",category);
+        }
     }
 }
