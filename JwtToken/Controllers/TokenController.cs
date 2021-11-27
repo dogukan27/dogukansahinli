e8f52538a0c74c665d703b4e8d2003ce5ffd2f2a
Doğukan Şahinli <44443599+dogukan27@users.noreply.github.com>
2021-11-27 19:47:23 +0200

    .Net Core Web Api JWT (JSON WEB TOKEN)

---
+++ JwtToken/Controllers/TokenController.cs
@@ -0,0 +1,55 @@
+﻿using JwtToken.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtToken.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetToken(User user) 
+        {
+            if (user.username=="admin" && user.password=="123")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("dogukaningizlianahtari");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[] { new Claim("isim", "dogukan") }),
+                    Expires = DateTime.Now.AddDays(7), //Token 7 günlük oldu
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+
+                return Ok(tokenHandler.WriteToken(token));
+            }
+
+            return BadRequest();
+                  
+        
+        }
+
+        [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet]
+        [Route("[action]")]
+        public string GetKorumali()
+        {
+            return "başarılı";
+        }
+
+
+    }
+}
